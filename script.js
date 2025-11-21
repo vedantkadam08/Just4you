1b423147598797d1520629333dc30754cdfb9711
Vivek kadam <vedantvkadam08@gmail.com>
2025-11-21 08:04:48 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // No special JS needed yet.
+function openPreview(src) {
+    document.getElementById("previewImg").src = src;
+    document.getElementById("imgPreview").style.display = "flex";
+}
+
+function closePreview() {
+    document.getElementById("imgPreview").style.display = "none";
+}
+
+function showPlaceholder(img) {
+    img.src = "images/placeholder.jpg";
+}
